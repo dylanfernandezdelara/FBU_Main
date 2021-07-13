cfdfe4489deda4b76d5475c04912ee60f57e5319
dylanfernandezdelara <dylanfdl@fb.com>
2021-07-13 17:50:02 -0500

    Food Truck data sign up saves to backend

--- findfood/View Controllers/FTruckSignUpViewController.h
+++ findfood/View Controllers/FTruckSignUpViewController.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FTruckSignUpViewController : UIViewController
+@interface FTruckSignUpViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
 
--- findfood/View Controllers/FTruckSignUpViewController.m
+++ findfood/View Controllers/FTruckSignUpViewController.m
@@ -6,8 +6,15 @@
 //
 
 #import "FTruckSignUpViewController.h"
+#import "Parse/Parse.h"
 
-@interface FTruckSignUpViewController ()
+@interface FTruckSignUpViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIButton *profilePhotoButton;
+@property (weak, nonatomic) IBOutlet UIButton *detailsPhotoButton;
+@property (weak, nonatomic) IBOutlet UITextField *foodTruckNameField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionField;
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
 
 @end
 
@@ -18,6 +25,62 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)saveNow:(UIButton *)sender {
+    PFUser *currUser =  [PFUser currentUser];
+    currUser[@"fullName"] = self.foodTruckNameField.text;
+    currUser[@"email"] = self.emailField.text;
+    currUser[@"userType"] = @"FoodTruck";
+    currUser[@"truckDescription"] = self.descriptionField.text;
+    NSData *imageData = UIImagePNGRepresentation(self.profilePhotoButton.currentImage);
+    PFFileObject *image = [PFFileObject fileObjectWithName:@"profilePhoto.png" data:imageData];
+    currUser[@"Image"] = image;
+    [[PFUser currentUser] saveInBackground];
+}
+- (IBAction)selectProfilePhotoNow:(UIButton *)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+- (IBAction)selectDetailsPhotoNow:(UIButton *)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    CGSize temp_size = CGSizeMake(600, 600);
+    UIImage *temp = [self resizeImage:editedImage withSize:temp_size];
+    [self.profilePhotoButton setImage:temp forState:UIControlStateNormal];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
 /*
 #pragma mark - Navigation
 
