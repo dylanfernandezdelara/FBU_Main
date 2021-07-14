6fd3009dc009eea14828fc9f6686335432ee6a3e
dylanfernandezdelara <dylanfdl@fb.com>
2021-07-14 10:53:45 -0500

    Added logged in user persistence

--- findfood/SceneDelegate.m
+++ findfood/SceneDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "Parse/Parse.h"
 
 @interface SceneDelegate ()
 
@@ -15,9 +16,16 @@
 
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    if (PFUser.currentUser) {
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        if ([@"FoodTruck" isEqualToString:PFUser.currentUser[@"userType"]]){
+            self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"foodTruckTabController"];
+        }
+        else if([@"generalUser" isEqualToString:PFUser.currentUser[@"userType"]]){
+            self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"userTabController"];
+        }
+    
+    }
 }
 
 
